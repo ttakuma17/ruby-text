23637f079e55722b17b0c85bb7c38b097821b367
ttakuma17 <ttanimiya@gmail.com>
2022-04-03 15:05:55 +0900

    配列とハッシュ

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,4 +1,27 @@
 def to_hex(r,g,b)
-	"#00000"
-	""
+	hex = "#"
+	# r.to_s(16).rjust(2,"0") +
+	# g.to_s(16).rjust(2,"0") +
+	# b.to_s(16).rjust(2,"0")
+	# [r,g,b].each do |n|
+	# 	hex += n.to_s(16).rjust(2, "0")
+	# end
+	# hex
+	[r,g,b].sum("#") do |n|
+		n.to_s(16).rjust(2, "0")
+	end
+end
+
+def to_ints(hex)
+	r = hex[1..2]
+	g = hex[3..4]
+	b = hex[5..6]
+	# ints = []
+	# [r,g,b].each do |s|
+	# 	ints << s.hex
+	# end
+	# ints
+	[r,g,b].map do |s|
+		s.hex
+	end
 end
